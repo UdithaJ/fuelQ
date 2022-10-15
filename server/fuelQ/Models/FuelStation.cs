9da4ceeb280a7f380f9a56f5147761016616b73e
MahendraThammita <dhmmpthammita@gmail.com>
2022-10-15 15:24:07 +0530

    Added models

---
+++ server/fuelQ/Models/FuelStation.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace fuelQ.Models
+{
+    [BsonIgnoreExtraElements]
+    public class FuelStation
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+        [BsonElement("address")]
+        public string Address { get; set; } = String.Empty;
+    }
+}
---
+++ server/fuelQ/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace fuelQ.Models
+{
+    [BsonIgnoreExtraElements]
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+
+        [BsonElement("nic")]
+        public string NIC { get; set; } = String.Empty;
+
+        [BsonElement("email")]
+        public string Email { get; set; } = String.Empty;
+
+        [BsonElement("password")]
+        public string Password { get; set; } = String.Empty;
+
+        [BsonElement("vehicleType")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string VehicleType { get; set; } = String.Empty;
+    }
+}
