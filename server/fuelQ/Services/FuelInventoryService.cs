d6da725d9810fc0db627bb4d01c73cd56e11aae5
MahendraThammita <dhmmpthammita@gmail.com>
2022-10-16 14:24:10 +0530

    Added Fuel Inventory controller and base services

---
+++ server/fuelQ/Services/FuelInventoryService.cs
@@ -0,0 +1,42 @@
+﻿using fuelQ.Helpers;
+using fuelQ.Models;
+using MongoDB.Driver;
+
+namespace fuelQ.Services
+{
+    public class FuelInventoryService : IFuelInventoryService
+    {
+        private readonly IMongoCollection<FuelInventory> _fuelInventory;
+
+        public FuelInventoryService(IfuelQDatabaseSetupHelper settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _fuelInventory = database.GetCollection<FuelInventory>(settings.FuelInventoryCollectionName);
+        }
+        public FuelInventory Create(FuelInventory fuelInventory)
+        {
+            _fuelInventory.InsertOne(fuelInventory);
+            return fuelInventory;
+        }
+
+        public List<FuelInventory> Get()
+        {
+            return _fuelInventory.Find(fuelInventory => true).ToList();
+        }
+
+        public FuelInventory Get(string id)
+        {
+            return _fuelInventory.Find(fuelInventory => fuelInventory.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _fuelInventory.DeleteOne(fuelInventory => fuelInventory.Id == id);
+        }
+
+        public void Update(string id, FuelInventory fuelInventory)
+        {
+            _fuelInventory.ReplaceOne(type => type.Id == id, fuelInventory);
+        }
+    }
+}
