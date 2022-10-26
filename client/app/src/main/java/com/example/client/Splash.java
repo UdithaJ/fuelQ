02b9a609903a91f41c84214a0344d4534837264e
UdithaJ <it19138114@my.sliit.lk>
2022-10-26 21:56:39 +0530

    splash screen

---
+++ client/app/src/main/java/com/example/client/Splash.java
@@ -0,0 +1,24 @@
+package com.example.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, 10000);
+    }
+}
